cd73a30013717a5f3bf4906d0d4cb0440ba24503
Alekseev Dmitriy <d.alekseev@talenttech.ru>
2021-05-14 05:06:56 -0400

    remove comments

--- protocheck.sh
+++ protocheck.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-#if [ -d "/github/workspace" ]
-#then
-#  echo "link dir /github/workspace/ to /protos/protos"
-#  ln -s /github/workspace/* /protos/protos
-#fi
-
 for i in `ls -1 /protos/*.sh`; do
   echo "=> execute check: `basename $i .sh | cut -c 3-` ..."
   if ! $i ; then
